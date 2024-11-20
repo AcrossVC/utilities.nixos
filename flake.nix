661a2c968ba0f3780e1840229a620165587f2ba6
AcrossVC <AcrossVC@gmail.com>
2024-11-19 21:31:38 -0500

    new flake, home-manager. didn't break my system yet... yay

--- flake.nix
+++ flake.nix
@@ -2,28 +2,49 @@
   description = "NixOS configuration";
 
   inputs = {
-    # Use nixos-24.05 to match your current system
+    # Stable system
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    # Add unstable for specific packages
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     
-    # Hardware support for your Lenovo Legion
+    # Keep hardware support if needed later
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
-    # We'll add home-manager after confirming this works
-    # home-manager.url = "github:nix-community/home-manager/release-24.05";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
-    nixosConfigurations = {
-      # Your hostname from configuration.nix
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-hardware, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      # Configure unstable overlay
+      overlay-unstable = final: prev: {
+        unstable = nixpkgs-unstable.legacyPackages.${system};
+      };
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
         modules = [
+          # Add overlay to system
+          { nixpkgs.overlays = [ overlay-unstable ]; }
+          
           # Your existing configuration
           ./configuration.nix
           
-          
+          # Home-manager
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = { inherit inputs; };
+              users.user = import ./home/user.nix;
+            };
+          }
         ];
       };
     };
-  };
 }
