2d75aa41a3f108436670564ec407a8e5b2bab263
AcrossVC <AcrossVC@gmail.com>
2024-11-19 19:53:18 -0500

    claude flake code init

--- flake.nix
+++ flake.nix
@@ -1,15 +1,30 @@
 {
-  description = "A very basic flake";
+  description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # Use nixos-24.05 to match your current system
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    
+    # Hardware support for your Lenovo Legion
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    # We'll add home-manager after confirming this works
+    # home-manager.url = "github:nix-community/home-manager/release-24.05";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
+    nixosConfigurations = {
+      # Your hostname from configuration.nix
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # Your existing configuration
+          ./configuration.nix
+          
+          # Hardware support for Lenovo Legion
+          nixos-hardware.nixosModules.lenovo-legion-15
+        ];
+      };
+    };
   };
 }
