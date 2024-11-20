ddd9989155a6c7d0a2b158d278c62c93ef038641
AcrossVC <AcrossVC@gmail.com>
2024-11-20 08:47:10 -0500

    still kinda broken. tried adding services back in the gnome module

--- modules/system/desktop-gnome.nix
+++ modules/system/desktop-gnome.nix
@@ -1,23 +1,47 @@
 # /etc/nixos/modules/system/desktop-gnome.nix
-
-# This module configures GNOME with theming and extensions
 { config, lib, pkgs, ... }:
 
 with lib;
 
 let 
   cfg = config.modules.desktop;
-  theme = cfg.theme;
 in {
   config = mkIf (cfg.enable && cfg.active == "gnome") {
-    # Enable GNOME Desktop Environment
-    services.xserver.desktopManager.gnome.enable = true;
+    # Enable X11 and GNOME
+    services.xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+      
+      # X11 keyboard layout
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
 
-    # Combined package list
+    # Enable sound with pipewire
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    # Required Gnome packages from your original config
+    services.xserver.excludePackages = [ pkgs.xterm ];
+    
     environment.systemPackages = with pkgs; [
       # GNOME utilities
       gnome.gnome-tweaks
+      gnome.dconf-editor
+      gnome-extension-manager
+      
+      # GNOME Shell Extensions
+      gnomeExtensions.user-themes
+      gnomeExtensions.dash-to-dock
+      gnomeExtensions.appindicator
     ];
-
   };
 }
